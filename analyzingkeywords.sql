1dfbf044e7a228d7f11ad247cefaaa9e58af7241
Daniel Eze <eze_d@yahoo.com>
2022-09-24 20:45:27 +0100

    Update analyzingkeywords.sql

--- analyzingkeywords.sql
+++ analyzingkeywords.sql
@@ -15,3 +15,27 @@ WHERE [Estimated CTR] IS NULL
 DELETE 
 FROM monthly_search_clean
 WHERE [Avg# monthly searches] IS NULL
+
+--We have to fill the empty values with 0
+UPDATE monthly_search_clean
+SET [Competition (indexed value)]= ISNULL([Competition (indexed value)], 0)
+--First, we create 2 new columns in the avg_monthly_search table, to fill with the clicks and CTR values of each keyword
+ALTER TABLE monthly_search_clean
+ADD Estimated_Clicks float
+
+ALTER TABLE monthly_search_clean
+ADD Estimated_CTR float
+
+--Then we fill the new columns with the data from the avg_CTR_clean table
+
+UPDATE a
+SET a.[Estimated_Clicks] = b.[Estimated Clicks]
+FROM monthly_search_clean a
+	left join avg_CTR_clean b
+	on a.Keyword=b.Keyword
+
+UPDATE a
+SET a.[Estimated_CTR] = b.[Estimated CTR]
+FROM monthly_search_clean a
+	left join avg_CTR_clean b
+	on a.Keyword=b.Keyword
